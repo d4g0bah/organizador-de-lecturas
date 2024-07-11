5e44dff5159c00dd7cd222b354d643e82b4b1796
d4g0bah <silence.thxguys@gmail.com>
2024-07-11 18:57:12 -0400

    Update ListaDeLectura.jsx

--- src/components/ListaDeLectura.jsx
+++ src/components/ListaDeLectura.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Libro from './Libro';
+import PropTypes from 'prop-types';
 
-const ListaDeLectura = ({ libros, eliminarLibro, editarLibro }) => {
+const ListaDeLectura = ({ libros, eliminarLibro, editarLibro, bloquearLibro, desbloquearLibro, esBloqueado }) => {
   return (
     <div>
       {libros.map(libro => (
-        <Libro key={libro.id} libro={libro} eliminarLibro={eliminarLibro} editarLibro={editarLibro} />
+        <Libro 
+          key={libro.id} 
+          libro={libro} 
+          eliminarLibro={eliminarLibro} 
+          editarLibro={editarLibro} 
+          bloquearLibro={bloquearLibro} 
+          desbloquearLibro={desbloquearLibro} 
+          esBloqueado={esBloqueado} 
+        />
       ))}
     </div>
   );
+};
+
+ListaDeLectura.propTypes = {
+  libros: PropTypes.array.isRequired,
+  eliminarLibro: PropTypes.func.isRequired,
+  editarLibro: PropTypes.func.isRequired,
+  bloquearLibro: PropTypes.func,
+  desbloquearLibro: PropTypes.func,
+  esBloqueado: PropTypes.bool
 };
 
 export default ListaDeLectura;
